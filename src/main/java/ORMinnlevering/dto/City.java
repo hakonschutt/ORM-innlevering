816bdb99dd68e0e3e330113a239404df29eb07ad
Hakon Schutt <hakon@schutt.no>
2017-11-05 17:11:26 +0100

    added dto for city and country

--- src/main/java/ORMinnlevering/dto/City.java
+++ src/main/java/ORMinnlevering/dto/City.java
@@ -1,7 +1,90 @@
 package ORMinnlevering.dto;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
 /**
  * Created by hakonschutt on 05/11/2017.
  */
+@DatabaseTable(tableName = "City")
 public class City {
+
+    public static final String NAME_FIELD_NAME = "Name";
+    public static final String COUNTRY_CODE_FIELD_NAME = "CountryCode";
+    public static final String DISTRICT_FIELD_NAME = "District";
+    public static final String POPULATION_FIELD_NAME = "Population";
+
+    @DatabaseField(generatedId = true, unique = true)
+    private int id;
+
+    @DatabaseField(columnName = NAME_FIELD_NAME, canBeNull = false)
+    private String name;
+
+    @DatabaseField(columnName = COUNTRY_CODE_FIELD_NAME, foreign = true, canBeNull = false)
+    private String countryCode;
+
+    @DatabaseField(columnName = DISTRICT_FIELD_NAME, canBeNull = false)
+    private String district;
+
+    @DatabaseField(columnName = POPULATION_FIELD_NAME, canBeNull = false)
+    private int population;
+
+    public City() {}
+
+    public City(int id, String name, String countryCode, String district, int population) {
+        this.id = id;
+        this.name = name;
+        this.countryCode = countryCode;
+        this.district = district;
+        this.population = population;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null || other.getClass() != getClass()) {
+            return false;
+        }
+
+        return id == ((City) other).id;
+    }
 }
--- src/main/java/ORMinnlevering/dto/Country.java
+++ src/main/java/ORMinnlevering/dto/Country.java
@@ -1,5 +1,6 @@
 package ORMinnlevering.dto;
 
+import ORMinnlevering.dto.support.Continent;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
@@ -19,12 +20,209 @@ public class Country {
     public static final String LIFE_EXPECTANCY_FIELD_NAME = "LifeExpectancy";
     public static final String GNP_FIELD_NAME = "GNP";
     public static final String GNP_OLD_FIELD_NAME = "GNPOld";
-    public static final String LOCALNAME_FIELD_NAME = "LocalName";
+    public static final String LOCAL_NAME_FIELD_NAME = "LocalName";
     public static final String GOVERNMENT_FORM_FIELD_NAME = "GovernmentForm";
     public static final String HEAD_OF_STATE_FIELD_NAME = "HeadOfState";
     public static final String CAPITAL_FIELD_NAME = "Capital";
     public static final String CODE_TWO_FIELD_NAME = "Code2";
 
-    @DatabaseField(columnName = COUNTRY_CODE_FIELD_NAME, uniqueCombo = true, foreign = true, canBeNull = false)
-    private String countryCode;
+    @DatabaseField(columnName = COUNTRY_CODE_FIELD_NAME, unique = true, canBeNull = false)
+    private String code;
+
+    @DatabaseField(columnName = NAME_FIELD_NAME, canBeNull = false)
+    private String name;
+
+    @DatabaseField(columnName = CONTINENT_FIELD_NAME, canBeNull = false)
+    private Continent continent;
+
+    @DatabaseField(columnName = REGION_FILED_NAME, canBeNull = false)
+    private String region;
+
+    @DatabaseField(columnName = SURFACE_AREA_FIELD_NAME, canBeNull = false)
+    private double surfaceArea;
+
+    @DatabaseField(columnName = INDEPYEAR_FIELD_NAME, canBeNull = false)
+    private short independenceYear;
+
+    @DatabaseField(columnName = POPULATION_FIELD_NAME, canBeNull = false)
+    private int population;
+
+    @DatabaseField(columnName = LIFE_EXPECTANCY_FIELD_NAME, canBeNull = false)
+    private double lifeExpectancy;
+
+    @DatabaseField(columnName = GNP_FIELD_NAME, canBeNull = false)
+    private double gnp;
+
+    @DatabaseField(columnName = GNP_OLD_FIELD_NAME, canBeNull = false)
+    private double gnpOld;
+
+    @DatabaseField(columnName = LOCAL_NAME_FIELD_NAME, canBeNull = false)
+    private String localName;
+
+    @DatabaseField(columnName = GOVERNMENT_FORM_FIELD_NAME, canBeNull = false)
+    private String governmentForm;
+
+    @DatabaseField(columnName = HEAD_OF_STATE_FIELD_NAME, canBeNull = false)
+    private String headOfState;
+
+    @DatabaseField(columnName = CAPITAL_FIELD_NAME, canBeNull = false)
+    private int captial;
+
+    @DatabaseField(columnName = CODE_TWO_FIELD_NAME, canBeNull = false)
+    private String secondCode;
+
+    public Country() {
+    }
+
+    public Country(String code, String name, Continent continent, String region, double surfaceArea, short independanceYear, int population, double lifeExpectancy, double gnp, double gnpOld, String localName, String governmentForm, String headOfState, int captial, String secondCode) {
+        this.code = code;
+        this.name = name;
+        this.continent = continent;
+        this.region = region;
+        this.surfaceArea = surfaceArea;
+        this.independenceYear = independanceYear;
+        this.population = population;
+        this.lifeExpectancy = lifeExpectancy;
+        this.gnp = gnp;
+        this.gnpOld = gnpOld;
+        this.localName = localName;
+        this.governmentForm = governmentForm;
+        this.headOfState = headOfState;
+        this.captial = captial;
+        this.secondCode = secondCode;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Continent getContinent() {
+        return continent;
+    }
+
+    public void setContinent(Continent continent) {
+        this.continent = continent;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public double getSurfaceArea() {
+        return surfaceArea;
+    }
+
+    public void setSurfaceArea(double surfaceArea) {
+        this.surfaceArea = surfaceArea;
+    }
+
+    public short getIndependenceYear() {
+        return independenceYear;
+    }
+
+    public void setIndependenceYear(short independenceYear) {
+        this.independenceYear = independenceYear;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public double getLifeExpectancy() {
+        return lifeExpectancy;
+    }
+
+    public void setLifeExpectancy(double lifeExpectancy) {
+        this.lifeExpectancy = lifeExpectancy;
+    }
+
+    public double getGnp() {
+        return gnp;
+    }
+
+    public void setGnp(double gnp) {
+        this.gnp = gnp;
+    }
+
+    public double getGnpOld() {
+        return gnpOld;
+    }
+
+    public void setGnpOld(double gnpOld) {
+        this.gnpOld = gnpOld;
+    }
+
+    public String getLocalName() {
+        return localName;
+    }
+
+    public void setLocalName(String localName) {
+        this.localName = localName;
+    }
+
+    public String getGovernmentForm() {
+        return governmentForm;
+    }
+
+    public void setGovernmentForm(String governmentForm) {
+        this.governmentForm = governmentForm;
+    }
+
+    public String getHeadOfState() {
+        return headOfState;
+    }
+
+    public void setHeadOfState(String headOfState) {
+        this.headOfState = headOfState;
+    }
+
+    public int getCaptial() {
+        return captial;
+    }
+
+    public void setCaptial(int captial) {
+        this.captial = captial;
+    }
+
+    public String getSecondCode() {
+        return secondCode;
+    }
+
+    public void setSecondCode(String secondCode) {
+        this.secondCode = secondCode;
+    }
+
+    @Override
+    public int hashCode() {
+        return code.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null || other.getClass() != getClass()) {
+            return false;
+        }
+
+        return code.equals(((Country) other).code);
+    }
 }
